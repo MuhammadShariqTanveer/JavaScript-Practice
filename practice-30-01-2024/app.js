830c34f2f2dbbbc8079edb4e3c7a374b723d3285
Muhammad Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2024-01-30 23:38:29 +0500

    Update app.js

---
+++ practice-30-01-2024/app.js
@@ -0,0 +1,15 @@
+var userName= prompt("Enter any keyword");
+console.log(userName.toLocaleUpperCase);
+
+ switch (userName) {
+    case "Shariq" :
+    alert("Han Yehi hai");
+    break;
+    
+    case "Raja" :
+    alert("Han yehi hai");
+     break;
+
+     default :
+     alert("Nhi hai");
+ }

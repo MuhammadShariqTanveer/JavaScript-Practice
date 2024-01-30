b78923ea96ce237b1a6aecbf38648ea81685ac21
Muhammad Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2024-01-30 23:43:20 +0500

    Update app.js

--- practice-30-01-2024/app.js
+++ practice-30-01-2024/app.js
@@ -1,15 +1,18 @@
-var userName= prompt("Enter any keyword");
-console.log(userName.toLocaleUpperCase);
-
- switch (userName) {
-    case "Shariq" :
-    alert("Han Yehi hai");
-    break;
+// var userName= prompt("Enter any keyword");
+  
+//  switch (userName) {
+//     case "Shariq" :
+//     alert("Han Yehi hai");
+//     break;
     
-    case "Raja" :
-    alert("Han yehi hai");
-     break;
+//     case "Raja" :
+//     alert("Han yehi hai");
+//      break;
 
-     default :
-     alert("Nhi hai");
- }
+//      default :
+//      alert("Nhi hai");
+//  }
+
+
+var array =["Muhammad" , "Shariq", "Tanveer", "Student"]
+console.log(array);

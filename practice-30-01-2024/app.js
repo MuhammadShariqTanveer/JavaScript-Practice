ed33dc7c6c82c4ce65c241c4ff2d0ecff49c1324
Muhammad Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2024-01-30 23:59:04 +0500

    Update app.js

--- practice-30-01-2024/app.js
+++ practice-30-01-2024/app.js
@@ -14,15 +14,15 @@
 //  }
 
 
-var myArray =["Muhammad" , "Shariq", "Tanveer", "Student"]
-console.log(myArray);
+// var myArray =["Muhammad" , "Shariq", "Tanveer", "Student"]
+// console.log(myArray);
 
-myArray.splice( 3,0, "Computer")
-console.log(myArray)
+// myArray.splice( 3,0, "Computer")
+// console.log(myArray)
 
- var newArray= myArray.slice(0,3);
- console.log(newArray);
+//  var newArray= myArray.slice(0,3);
+//  console.log(newArray);
 
- console.log(`${newArray[1]} is a good boy, his father name is ${newArray[2]} and he is a police officer in pakistan. `)
- console.log(`${newArray} is a ${myArray[3]} ${myArray[4]} in Smiu`)
+//  console.log(`${newArray[1]} is a good boy, his father name is ${newArray[2]} and he is a police officer in pakistan. `);
+//  console.log(`${newArray} is a ${myArray[3]} ${myArray[4]} in Smiu`);
 
